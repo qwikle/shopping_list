5c7ed0e4d20621a3b336bfdb0b40de8db822f8cb
Rayane Kettana <rayane.kettana@visiativ.com>
2022-05-26 19:09:33 +0200

    feat <config-dart> : added configuration of input appearance

--- lib/app/config/config.dart
+++ lib/app/config/config.dart
@@ -3,9 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 String apiUrl =
     !Platform.isAndroid ? 'http://127.0.0.1:3333' : 'http://10.0.2.2:3333';
+
+const Color primaryColor = Color(0xff6750A4);
+BorderRadius borderRadius = BorderRadius.circular(30.h);
 
 ThemeData theme = ThemeData(
   useMaterial3: true,
@@ -15,8 +19,19 @@ ThemeData theme = ThemeData(
     titleTextStyle: GoogleFonts.poppins(color: Colors.black),
     centerTitle: false,
   ),
-  primaryColor: const Color(0xff6750A4),
-
+  primaryColor: primaryColor,
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: const TextStyle(
+      color: primaryColor
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: primaryColor),
+      borderRadius: borderRadius,
+    ),
+    border: OutlineInputBorder(
+      borderRadius: borderRadius,
+    ),
+  ),
 );
 
 ThemeData darkTheme = ThemeData(scaffoldBackgroundColor: Colors.black26);
