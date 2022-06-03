c9f9f6e52e2d3c1df8d6535220fb31c9b7c31129
Qwikle <qwikle.ziko@gmail.com>
2022-06-03 16:04:31 +0200

    refractor<model> put model in the right folders

---
+++ lib/app/data/models/sign_up_model.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'sign_up_model.g.dart';
+
+@JsonSerializable()
+class SignUp extends Equatable {
+  final String email;
+  final String emailConfirmation;
+  final String password;
+  final String passwordConfirmation;
+  final String firstName;
+  final String lastName;
+  final String birthDay;
+
+  const SignUp({
+    required this.email,
+    required this.emailConfirmation,
+    required this.password,
+    required this.passwordConfirmation,
+    required this.firstName,
+    required this.lastName,
+    required this.birthDay,
+  });
+
+  factory SignUp.fromJson(Map<String, dynamic> json) => _$SignUpFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignUpToJson(this);
+
+  @override
+  List<Object?> get props => [
+        email,
+        emailConfirmation,
+        password,
+        passwordConfirmation,
+        firstName,
+        lastName,
+      ];
+}
