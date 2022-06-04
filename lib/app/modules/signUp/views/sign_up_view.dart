93632f31f557059592173781dfecf16f0322f590
Qwikle <qwikle.ziko@gmail.com>
2022-06-04 23:20:45 +0200

    refactor<sign up> separate forms

--- lib/app/modules/signUp/views/sign_up_view.dart
+++ lib/app/modules/signUp/views/sign_up_view.dart
@@ -1,79 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:sizer/sizer.dart';
 
-import '../../../components/forms/confirmation_input_view.dart';
-import '../../../components/forms/email_input_view.dart';
-import '../../../components/forms/password_input_view.dart';
-import '../../../config/types.dart';
 import '../controllers/sign_up_controller.dart';
+import 'information_view.dart';
+import 'profile_view.dart';
 
 class SignUpView extends GetView<SignUpController> {
   const SignUpView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Sign up'),
-        ),
-        body: SingleChildScrollView(
-          child: Align(
-            alignment: const Alignment(0, -1 / 3),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.w),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Welcome to\nMy Shopping List Apps',
-                    style: GoogleFonts.nunitoSans(
-                      fontSize: 3.h,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10.h),
-                  SvgPicture.asset(
-                    'assets/svg/sign-up-mail.svg',
-                    height: 20.h,
-                    fit: BoxFit.contain,
-                  ),
-                  SizedBox(height: 3.h),
-                  EmailInputView(
-                    input: controller.email,
-                    label: 'Adresse mail',
-                    onChanged: controller.onEmailChanged,
-                  ),
-                  SizedBox(height: 2.h),
-                  ConfirmationInputView(
-                    input: controller.emailConfirmation,
-                    onChanged: controller.onEmailConfirmationChanged,
-                    label: 'Confirmez votre adresse mail',
-                  ),
-                  SizedBox(height: 2.h),
-                  PasswordInputView(
-                    input: controller.password,
-                    label: 'Mot de passe',
-                    controller: controller.passwordController,
-                    onChanged: controller.onPasswordChanged,
-                    obscure: controller.obscure,
-                  ),
-                  SizedBox(height: 2.h),
-                  ConfirmationInputView(
-                    input: controller.passwordConfirmation,
-                    onChanged: controller.onPasswordConfirmationChanged,
-                    label: 'Confirmez votre mot de passe',
-                    obscure: controller.obscure,
-                    type: ConfirmationType.password,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ));
+    return Obx(
+      () => controller.page.value == SignUpPage.information
+          ? const InformationView()
+          : const ProfileView(),
+    );
   }
 }
