9140dd8fbff3243c507a8c6ede7e35d9c648a9b3
Qwikle <qwikle.ziko@gmail.com>
2022-06-04 19:20:39 +0200

    refactor<sign in>  reformat file

--- lib/app/modules/signIn/providers/sign_in_provider.dart
+++ lib/app/modules/signIn/providers/sign_in_provider.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:get/get.dart';
@@ -14,16 +13,16 @@ class SignInProvider extends GetConnect {
   }
 
   Future<Token> signIn(String email, String password) async {
-      final payload = {'email': email, 'password': password};
+    final payload = {'email': email, 'password': password};
 
-      final response = await post('/login', payload);
+    final response = await post('/login', payload);
 
-      if (response.statusCode == 200) {
-        return Token.fromJson(response.body);
-      }
-      if (response.statusCode == 400) {
-        throw SignInFailureException.fromCode(response.statusCode!);
-      }
-      throw const SocketException('Fail to connect to the server');
+    if (response.statusCode == 200) {
+      return Token.fromJson(response.body);
+    }
+    if (response.statusCode == 400) {
+      throw SignInFailureException.fromCode(response.statusCode!);
+    }
+    throw const SocketException('Fail to connect to the server');
   }
 }
