b2853d41f0ba0de7a34b096057c1879c9f65badd
Qwikle <qwikle.ziko@gmail.com>
2022-06-04 19:21:52 +0200

    refactor<widget> add toggle method inside widget

--- lib/app/components/widgets/obscure_text_icon.dart
+++ lib/app/components/widgets/obscure_text_icon.dart
@@ -6,21 +6,21 @@ class ObscureText extends StatelessWidget {
   const ObscureText({
     Key? key,
     required this.obscure,
-    required this.onTap,
   }) : super(key: key);
 
   final Rx<bool> obscure;
-  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: obscure.value
-          ? const Icon(Icons.visibility_off)
-          : const Icon(Icons.visibility),
-      onTap: onTap,
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
+    return Obx(
+      () => InkWell(
+        child: obscure.value
+            ? const Icon(Icons.visibility_off)
+            : const Icon(Icons.visibility),
+        onTap: () => obscure.toggle(),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
     );
   }
 }
