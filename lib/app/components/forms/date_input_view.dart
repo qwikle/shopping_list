ea76d08bffe29a8938e8b6a8095f48ef54f9758e
Qwikle <qwikle.ziko@gmail.com>
2022-06-06 19:29:18 +0200

    feat<input> rename file and change validation

---
+++ lib/app/components/forms/date_input_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:shopping_list/app/inputs/date_input.dart';
+
+import '../widgets/clear_suffix_icon.dart';
+
+class DateInputView extends StatelessWidget {
+  DateInputView({
+    Key? key,
+    required this.input,
+    required this.label,
+    required this.onChanged,
+  }) : super(key: key) {
+    controller.text = input.value.value;
+  }
+
+  _errorMessage() {
+    if (input.value.invalid) {
+      if (input.value.error == DateValidationError.empty) {
+        return 'Veuillez indiquer votre $label.';
+      }
+      if (input.value.error == DateValidationError.invalid) {
+        return 'Veuillez indiquer une date valide.';
+      }
+      if (input.value.error == DateValidationError.outOfRange) {
+        return 'Veuillez indiquer une date valide.';
+      }
+      if (input.value.error == DateValidationError.youngerThan18) {
+        return 'Vous devez avoir 18 ans pour vous inscrire.';
+      }
+    }
+    return null;
+  }
+
+  final Rx<DateInput> input;
+  final TextEditingController controller = TextEditingController();
+  final void Function(String) onChanged;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => TextField(
+        controller: controller,
+        keyboardType: TextInputType.datetime,
+        onChanged: onChanged,
+        maxLength: 10,
+        decoration: InputDecoration(
+          labelText: label,
+          counterText: '',
+          hintText: 'jj/mm/aaaa',
+          errorText: _errorMessage(),
+          suffixIcon: ClearSuffixIcon(
+            onTap: () {
+              input.value = DateInput.dirty(value: '');
+              controller.clear();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
