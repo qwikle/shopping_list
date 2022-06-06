dd538630229c349a292401468eb0a452be7d92d6
Qwikle <qwikle.ziko@gmail.com>
2022-06-06 19:33:34 +0200

    feat<config> add utils class

--- lib/app/config/types.dart
+++ lib/app/config/types.dart
@@ -1,3 +1,6 @@
+import 'package:get/utils.dart';
+import 'package:intl/intl.dart';
+
 enum PasswordType {
   createPassword,
   existPassword,
@@ -9,4 +12,19 @@ enum ConfirmationType {
   email,
   number,
   date,
+}
+
+enum DateType {
+  birthDay,
+  range,
+}
+
+class Utils {
+  static String formatDate(DateTime date) {
+    final format = DateFormat('dd/MM/yyyy');
+    return format.format(date);
+  }
+
+  static isFrenchDate(String value) =>
+      GetUtils.hasMatch(value, r'^[0-9]{2}/[0-9]{2}/[0-9]{4}$');
 }
