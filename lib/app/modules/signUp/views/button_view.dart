a15818854ee562004bed38b0fb75ef37483b93bc
Qwikle <qwikle.ziko@gmail.com>
2022-06-06 17:17:00 +0200

    fix<sign up> fix button don't change on valid status

--- lib/app/modules/signUp/views/button_view.dart
+++ lib/app/modules/signUp/views/button_view.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:formz/formz.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/sign_up_controller.dart';
 
 class ButtonView extends GetView<SignUpController> {
-  const ButtonView({Key? key, required this.onPressed}) : super(key: key);
+  const ButtonView({Key? key, required this.onPressed, required this.status}) : super(key: key);
 
-  final void Function()? onPressed;
+  final Rx<FormzStatus> status;
+  final void Function() onPressed;
   @override
   Widget build(BuildContext context) {
     return Obx(
       () => ElevatedButton(
-        onPressed: onPressed,
+        onPressed: status.value.isValid ? onPressed : null,
         child: controller.page.value == SignUpPage.information
             ? const Text('Suivant')
             : const Text('S\'inscrire'),
