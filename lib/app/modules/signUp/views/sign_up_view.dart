da3c0a1ff3a3c339167c5e09d15e1a186e7191c0
Qwikle <qwikle.ziko@gmail.com>
2022-06-06 17:14:24 +0200

    refactor<sign up> set the scaffold on mainpage

--- lib/app/modules/signUp/views/sign_up_view.dart
+++ lib/app/modules/signUp/views/sign_up_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:sizer/sizer.dart';
 
 import '../controllers/sign_up_controller.dart';
 import 'information_view.dart';
@@ -11,10 +12,23 @@ class SignUpView extends GetView<SignUpController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => controller.page.value == SignUpPage.information
-          ? const InformationView()
-          : const ProfileView(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: const Alignment(0, -1 / 3),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: Obx(
+              () => controller.page.value == SignUpPage.information
+                  ? const InformationView()
+                  : const ProfileView(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
