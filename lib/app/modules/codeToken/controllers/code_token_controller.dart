6ec4be28bee553cd518069e6afd0d0fd5ebb256b
Qwikle <qwikle.ziko@gmail.com>
2022-06-25 01:01:32 +0200

    feat checkCode method

--- lib/app/modules/codeToken/controllers/code_token_controller.dart
+++ lib/app/modules/codeToken/controllers/code_token_controller.dart
@@ -1,6 +1,9 @@
 import 'package:formz/formz.dart';
 import 'package:get/get.dart';
-import 'package:shopping_list/app/inputs/code_input.dart';
+
+import '../../../inputs/code_input.dart';
+import '../../../routes/app_pages.dart';
+import '../providers/codetoken_provider.dart';
 
 class CodeTokenController extends GetxController {
   final Rx<FormzStatus> status = FormzStatus.pure.obs;
@@ -10,8 +13,9 @@ class CodeTokenController extends GetxController {
   final Rx<CodeInput> codeFour = const CodeInput.pure().obs;
   final Rx<CodeInput> codeFive = const CodeInput.pure().obs;
   final Rx<CodeInput> codeSix = const CodeInput.pure().obs;
-
-  _validateStatus() {
+  final Rx<String> email = ''.obs;
+  final Rx<String> type = ''.obs;
+  _validateStatus() async {
     status.value = Formz.validate([
       codeOne.value,
       codeTwo.value,
@@ -20,6 +24,14 @@ class CodeTokenController extends GetxController {
       codeFive.value,
       codeSix.value,
     ]);
+
+    if (status.value == FormzStatus.valid) {
+      await checCode();
+    }
+  }
+
+  _getCode() {
+    return '${codeOne.value.value}${codeTwo.value.value}${codeThree.value.value}${codeFour.value.value}${codeFive.value.value}${codeSix.value.value}';
   }
 
   onCodeOneChanged(String value) {
@@ -50,5 +62,24 @@ class CodeTokenController extends GetxController {
   onCodeSixChanged(String value) {
     codeSix.value = CodeInput.dirty(value: value);
     _validateStatus();
+  }
+
+  checCode() async {
+    status.value = FormzStatus.submissionInProgress;
+    try {
+      await Get.find<CodetokenProvider>().checkCode(email.value, _getCode());
+      if (type.value == 'check_email') {
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        Get.offAllNamed(Routes.RESET_PASSWORD, arguments: {
+          'email': email.value,
+          'code': _getCode(),
+        });
+      }
+    } catch (e) {
+      status.value = FormzStatus.submissionFailure;
+      Get.closeAllSnackbars();
+      Get.snackbar('Erreur', e.toString());
+    }
   }
 }
