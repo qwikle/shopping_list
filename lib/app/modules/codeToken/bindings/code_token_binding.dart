dd3ecc90d2730530f31db4d1e43edb94c1d3878b
Qwikle <qwikle.ziko@gmail.com>
2022-06-25 01:00:44 +0200

    feat add provider lazy import

--- lib/app/modules/codeToken/bindings/code_token_binding.dart
+++ lib/app/modules/codeToken/bindings/code_token_binding.dart
@@ -1,12 +1,16 @@
 import 'package:get/get.dart';
 
 import '../controllers/code_token_controller.dart';
+import '../providers/codetoken_provider.dart';
 
 class CodeTokenBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<CodeTokenController>(
       () => CodeTokenController(),
+    );
+    Get.lazyPut<CodetokenProvider>(
+      () => CodetokenProvider(),
     );
   }
 }
